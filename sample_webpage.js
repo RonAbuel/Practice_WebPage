afd05640dfb77c70f05d1ec289d965c7b11cca23
Ron <ron848426@gmail.com>
2020-09-09 22:19:54 -0700

    added toggle function on hamburger menu

--- sample_webpage.js
+++ sample_webpage.js
@@ -3,14 +3,18 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-
 const hamburger = document.getElementById ('hamburger');
 
-const side_menu2 = document.getElementById('toggle_menu')
+const side_menu2 = document.getElementById('toggle_menu');
+
+const side_menu1 = document.getElementById('side_menu_id');
 
 hamburger.addEventListener('click', () => {
 	side_menu2.classList.toggle('toggle_hamburger_menu');
+
+	side_menu1.classList.toggle('toggle_hamburger_menu2');
 });
+
 
 
 
